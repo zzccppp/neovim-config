f5405034e67e8ac2cf2d22bbcd26cc20c5dcc76e
jinzhongjia <jinzhongjia@qq.com>
2023-04-09 01:47:19 +0800

    Update: use sqlls to replace sqls

--- lua/plugin-config/mason/lsp/list.lua
+++ lua/plugin-config/mason/lsp/list.lua
@@ -17,7 +17,7 @@ local list = {
 	-- { name = "pyright", alone = false },
 	{ name = "jedi_language_server", alone = false },
 	-- { name = "pylsp", alone = false },
-	{ name = "sqls", alone = false }, -- need extra yaml configuration
+	{ name = "sqlls", alone = false }, -- need extra yaml configuration
 	-- { name = "tailwindcss", alone = false },
 	{ name = "volar", alone = false }, -- Can be customized to override the default tsserver
 	{ name = "marksman", alone = false },
